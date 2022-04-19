dc00eed2b507065dfaf0460095e310b750be03d7
nobard <sashayelizarov2001@gmail.com>
2022-04-20 01:45:22 +0500

    1 try

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ class Navigator {
    * @param {*} cities 
    */
   constructor(cities) {
+    if(!Array.isArray(cities)){
+      throw new Error('Некорректные данные');
+    }
+
+    this.cities = cities;
   }
 
   /**
@@ -14,6 +19,52 @@ class Navigator {
    * @param {number} consumtion
    */
   buildPath(pointA, pointB, consumtion) {
+    if(typeof pointA !== 'string' 
+      || typeof pointB !== 'string'
+      || typeof consumtion !== 'number') {
+        throw new Error('Некорректные данные');
+      }
+
+    let result = null;
+
+    const findBestPath = (currentCity, visitedCities = [], distance = 0, currSum = 0) => {
+      currentCity = this.cities.find(e => e.name === currentCity);
+
+      if(currentCity){
+        let sortedCities = Object.entries(currentCity.paths).sort(([, aDist], [, bDist]) => aDist - bDist).reduce((p, [key, value]) => {
+          p[key] = value;
+          return p;
+        } , {});
+
+        visitedCities.push(currentCity.name);
+
+        for(const city in sortedCities){
+          if(visitedCities.includes(city)) continue;
+
+          const dist = sortedCities[city];
+          const sum = currentCity.petrolPrice * dist;
+          const finalDistSum = findBestPath(city, visitedCities, distance + dist, currSum + sum);
+
+          if(typeof finalDistSum === 'object'){
+            if(!result || result.distance > finalDistSum.distance){
+              result = {
+                distance: finalDistSum.distance,
+                sum: finalDistSum.currSum * consumtion
+              }
+            }
+
+            break;
+          }
+        }
+      }
+
+      if(currentCity?.name === pointB) return { distance, currSum };
+      return;
+    }
+
+    findBestPath(pointA);
+
+    return result;
   }
 }
 
