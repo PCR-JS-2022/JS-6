b6f1a789db9f1fd6531c707def4f1f2252022143
Semen Druzhinin <sema0118@gmail.com>
2022-04-19 21:41:03 +0500

    Семен Дружинин

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Navigator {
    * @param {*} cities 
    */
   constructor(cities) {
+    this.cities = cities;
   }
 
   /**
@@ -14,7 +15,47 @@ class Navigator {
    * @param {number} consumtion
    */
   buildPath(pointA, pointB, consumtion) {
+    if (typeof pointA !== 'string' || !pointA || typeof pointB !== 'string' || !pointB)
+      throw new Error('Неверно задан путь')
+
+    if (typeof consumtion !== 'number' || !pointB)
+      throw new Error('Топливо расходуется неверно')
+
+    const tempCities = this.cities.map(city => ({ ...city, visited: false }))
+    let result = {
+      distance: Number.MAX_VALUE
+    }
+
+    const getAllPaths = (currentCity, distance, sum) => {
+
+
+      if (currentCity.visited || currentCity.name === pointB) {
+        if (currentCity.name === pointB && result.distance > distance) {
+          result = { distance, sum }
+        }
+        return
+      }
+
+      currentCity.visited = true
+
+      for (const town in currentCity.paths) {
+        const nextCity = tempCities.find(c => c.name === town)
+        const currentDistance = distance + currentCity.paths[town]
+        const currentPrice = sum + currentCity.paths[town] * currentCity.petrolPrice
+
+        getAllPaths(nextCity, currentDistance, currentPrice)
+      }
+    }
+    getAllPaths(tempCities.find(city => city.name === pointA), 0, 0)
+
+    if (result == { distance: Number.MAX_VALUE }) {
+      throw new Error('Пути из A в B нет')
+    }
+    result = {
+      distance: result.distance,
+      sum: result.sum * consumtion
+    }
+    return result
   }
 }
-
 module.exports = { Navigator };
