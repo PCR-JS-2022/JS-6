bff7d912d3bd641ea5e4fa9f32e29f44f907a880
device <20yaroslav0131@gmail.com>
2022-04-19 15:03:11 +0500

    Поправил код от тестов

--- index.js
+++ index.js
@@ -55,45 +55,4 @@ class Navigator {
     }
 }
 
-const cities = [
-    {
-      name: "Yekaterinburg",
-      petrolPrice: 50.0,
-      paths: {
-        Chelyabinsk: 200,
-        Tumen: 350,
-      },
-    },
-    {
-      name: "Perm",
-      petrolPrice: 46.0,
-      paths: {
-        Yekaterinburg: 300,
-        Chelyabinsk: 500,
-        Tumen: 650,
-      },
-    },
-    {
-      name: "Chelyabinsk",
-      petrolPrice: 42.45,
-      paths: {
-        Yekaterinburg: 400,
-        Perm: 700,
-        Tumen: 748,
-      },
-    },
-    {
-      name: "Tumen",
-      petrolPrice: 60.45,
-      paths: {
-        Yekaterinburg: 9,
-        Perm: 780,
-        Chelyabinsk: 130,
-      },
-    },
-  ];
-  
-  const nav = new Navigator(cities);
-  console.log(nav.buildPath("Chelyabinsk", "Tumen", 0.06));
-
 module.exports = { Navigator };
