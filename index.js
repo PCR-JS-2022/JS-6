1b78a90b8b6279afb3b793ae6272187826a27a99
apterousteen <ayzhan312@live.ru>
2022-04-20 09:35:18 +0500

    1st try

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ class Navigator {
    * @param {*} cities 
    */
   constructor(cities) {
+    if (!Array.isArray(cities) || cities.every(city => typeof city !== 'object')) {
+      throw new TypeError()
+    };
+
+    this.cities = cities;
   }
 
   /**
@@ -14,6 +19,40 @@ class Navigator {
    * @param {number} consumtion
    */
   buildPath(pointA, pointB, consumtion) {
+    if (typeof pointA !== 'string' || typeof pointB !== 'string' || typeof consumtion !== 'number') {
+      throw new TypeError()
+    }
+
+    let startVertex = this.cities.find(city => city.name === pointA);
+    let finishVertex = this.cities.find(city => city.name === pointB);
+    let visited = {};
+    let result = [];
+
+    const findPath = (activeVertex, pathLength, tempCost) => {
+      if (!visited.hasOwnProperty(activeVertex.name)) {
+        if (activeVertex === finishVertex) {
+          result.push({ distance: pathLength, sum: tempCost });
+          return;
+        }
+
+        visited[activeVertex.name] = 1;
+
+        for (let path in activeVertex.paths) {
+          let nextVertex = this.cities.find(c => c.name === path);
+          let distance = activeVertex.paths[path];
+          let cost = distance * consumtion * activeVertex.petrolPrice;
+          findPath(nextVertex, pathLength + distance, tempCost + cost);
+        }
+      }
+    }
+
+    findPath(startVertex, 0, 0);
+
+    if (result.length === 0) {
+      throw new Error("There is no path between cities");
+    }
+
+    return result.sort((first, second) => first.distance - second.distance)[0];
   }
 }
 
