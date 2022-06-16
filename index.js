aade9571a7942932516b526fa702ee4499b43112
65bar <65BobrovMA@region.cbr.ru>
2022-06-16 14:06:56 +0500

    Выполнено задание JS 6

--- index.js
+++ index.js
@@ -5,16 +5,57 @@ class Navigator {
    * @param {*} cities 
    */
   constructor(cities) {
+    this.cities = cities;
   }
 
   /**
    * Ищет кратчайший путь от точки А до точки B
    * @param {string} pointA 
    * @param {string} pointB 
-   * @param {number} consumtion
+   * @param {number} consumtion - расход
    */
   buildPath(pointA, pointB, consumtion) {
+    if (
+      typeof pointA !== "string"  ||
+      typeof pointB !== "string" ||
+      typeof consumtion !== "number"
+    ) {
+      throw new Error("Неверный путь");
+    }
+
+    let citiesName = this.cities.map((city) => ({ ...city, Visited: false }));
+    let result = [];
+
+    let findAWays = (cityName, distance, sum) => {
+      let nameCity = citiesName.find((c) => c.name === cityName);
+      let cityEnd = this.cities.find((city) => city.name === pointB);
+
+      if (cityName === cityEnd.name) {
+        result.push({ distance, sum });
+        return;
+      }
+
+      if (nameCity.Visited) {
+        return;
+      }
+      nameCity.Visited = true;
+
+      for (let currentNmaeOfCity in nameCity.paths) {
+        let currentDistance = distance + nameCity.paths[currentNmaeOfCity];
+        let currentSum =
+          sum +
+          nameCity.paths[currentNmaeOfCity] * nameCity.petrolPrice * consumtion;
+        findAWays(currentNmaeOfCity, currentDistance, currentSum);
+      }
+    };
+
+    let cityStart = this.cities.find((city) => city.name === pointA);
+    findAWays(cityStart.name, 0, 0);
+
+    if (result.length === 0) throw new Error("Нет пути из города A в город B");
+    return result.sort((a, b) => a.distance - b.distance)[0];
   }
+  
 }
 
 module.exports = { Navigator };
